8accef7e64c7689c84cdce5e16350c899ba4f9ba
Alexander Causey <alexander.w.causey@gmail.com>
2021-08-17 10:29:12 -0700

    brute force solution for three number sum

---
+++ ThreeNumberSum.js
@@ -0,0 +1,17 @@
+function threeNumberSum(array, targetSum) {
+	const results = [];
+	array.sort((a, b) => a - b);
+  for (let i = 0; i < array.length; i++) {
+		for (let j = i + 1; j < array.length; j++) {
+			for (let k = j + 1; k < array.length; k++) {
+				if (array[i] + array[j] + array[k] === targetSum) {
+					results.push([array[i], array[j], array[k]]);
+				}
+			}
+		}
+	}
+	return results;
+}
+
+//time: O(n^4)
+//space: O(n)
